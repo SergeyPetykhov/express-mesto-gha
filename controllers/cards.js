6939df09a2d1f93a778f5fd9cc9f3534a32f0049
Sergey Petykhov <sergey_petykhov@mail.ru>
2023-05-11 10:28:46 +0300

    practice work #14

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,24 +1,28 @@
 const {
   CREATED_CODE,
   BAD_REQUEST_ERROR_CODE,
+  UNAUTHORIZED_ERROR_CODE,
   NOT_FOUND_ERROR_CODE,
   INTERNAL_SERVER_ERROR_CODE,
 } = require('../constants/constants');
 
+const AuthorizationError = require('../errors/AuthorizationError');
+
 const Card = require('../models/cards');
 
-const getCards = (req, res) => {
+const getCards = (req, res, next) => {
   Card.find()
     .populate(['owner', 'likes'])
     .then((cards) => {
       res.send({ data: cards });
     })
-    .catch(() => {
-      res.status(INTERNAL_SERVER_ERROR_CODE).send({ message: 'Произошла неизвестная ошибка на сервере' });
-    });
+    .catch(next);
+    // .catch(() => {
+    //   res.status(INTERNAL_SERVER_ERROR_CODE).send({ message: 'Произошла неизвестная ошибка на сервере' });
+    // });
 };
 
-const createCard = (req, res) => {
+const createCard = (req, res, next) => {
   const { name, link } = req.body;
   const ownerId = req.user._id;
 
@@ -26,37 +30,64 @@ const createCard = (req, res) => {
     .then((newCard) => {
       res.status(CREATED_CODE).send({ data: newCard });
     })
-    .catch((err) => {
-      if (err.name === 'ValidationError') {
-        res.status(BAD_REQUEST_ERROR_CODE).send({ message: 'Переданы некорректные данные при создании карточки' });
-      } else {
-        res.status(INTERNAL_SERVER_ERROR_CODE).send({ message: 'Произошла неизвестная ошибка на сервере' });
-      }
-    });
+    .catch(next);
+    // .catch((err) => {
+    //   if (err.name === 'ValidationError') {
+    //     res.status(BAD_REQUEST_ERROR_CODE).send({ message: 'Переданы некорректные данные при создании карточки' });
+    //   } else {
+    //     res.status(INTERNAL_SERVER_ERROR_CODE).send({ message: 'Произошла неизвестная ошибка на сервере' });
+    //   }
+    // });
 };
 
-const deleteСard = (req, res) => {
+const deleteСard = (req, res, next) => {
   const { cardId } = req.params;
+  const userId = req.user._id;
 
-  Card.findByIdAndRemove(cardId)
+  Card.findById(cardId)
     .orFail()
-    .then(() => {
-      res.send({ message: 'Карточка удалена' });
+    .populate(['owner', 'likes'])
+    .then((card) => {
+      const ownerId = card.owner._id.toString();
+
+      if (userId === ownerId) {
+        Card.findByIdAndRemove(cardId)
+          .orFail()
+          .then(() => {
+            res.send({ message: 'Карточка удалена' });
+          })
+          .catch(next);
+          // .catch((err) => {
+          //   if (err.name === 'DocumentNotFoundError') {
+          //     res.status(NOT_FOUND_ERROR_CODE).send({ message: 'Передан несуществующий _id карточки' });
+          //     return;
+          //   }
+          //   if (err.name === 'CastError') {
+          //     res.status(BAD_REQUEST_ERROR_CODE).send({ message: 'Передан некорректный _id карточки' });
+          //     return;
+          //   }
+          //   res.status(INTERNAL_SERVER_ERROR_CODE).send({ message: 'Произошла неизвестная ошибка на сервере' });
+          // });
+      } else {
+       // res.status(UNAUTHORIZED_ERROR_CODE).send({ message: 'Нет прав для удаления этой карточки' });
+       return next(new AuthorizationError('Нет прав для удаления этой карточки'));
+      }
     })
-    .catch((err) => {
-      if (err.name === 'DocumentNotFoundError') {
-        res.status(NOT_FOUND_ERROR_CODE).send({ message: 'Передан несуществующий _id карточки' });
-        return;
-      }
-      if (err.name === 'CastError') {
-        res.status(BAD_REQUEST_ERROR_CODE).send({ message: 'Передан некорректный _id карточки' });
-        return;
-      }
-      res.status(INTERNAL_SERVER_ERROR_CODE).send({ message: 'Произошла неизвестная ошибка на сервере' });
-    });
+    .catch(next);
+    // .catch((err) => {
+    //   if (err.name === 'DocumentNotFoundError') {
+    //     res.status(NOT_FOUND_ERROR_CODE).send({ message: 'Передан несуществующий _id карточки' });
+    //     return;
+    //   }
+    //   if (err.name === 'CastError') {
+    //     res.status(BAD_REQUEST_ERROR_CODE).send({ message: 'Передан некорректный _id карточки' });
+    //     return;
+    //   }
+    //   res.status(INTERNAL_SERVER_ERROR_CODE).send({ message: 'Произошла неизвестная ошибка на сервере' });
+    // });
 };
 
-const likeCard = (req, res) => {
+const likeCard = (req, res, next) => {
   const userId = req.user._id;
   const { cardId } = req.params;
 
@@ -65,24 +96,25 @@ const likeCard = (req, res) => {
     .then((card) => {
       res.send({ data: card });
     })
-    .catch((err) => {
-      if (err.name === 'DocumentNotFoundError') {
-        res.status(NOT_FOUND_ERROR_CODE).send({ message: 'Передан несуществующий _id карточки' });
-        return;
-      }
-      if (err.name === 'CastError') {
-        res.status(BAD_REQUEST_ERROR_CODE).send({ message: 'Передан некорректный _id карточки' });
-        return;
-      }
-      if (err.name === 'ValidationError') {
-        res.status(BAD_REQUEST_ERROR_CODE).send({ message: 'Переданы некорректные данные для постановки лайка' });
-        return;
-      }
-      res.status(INTERNAL_SERVER_ERROR_CODE).send({ message: 'Произошла неизвестная ошибка на сервере' });
-    });
+    .catch(next);
+    // .catch((err) => {
+    //   if (err.name === 'DocumentNotFoundError') {
+    //     res.status(NOT_FOUND_ERROR_CODE).send({ message: 'Передан несуществующий _id карточки' });
+    //     return;
+    //   }
+    //   if (err.name === 'CastError') {
+    //     res.status(BAD_REQUEST_ERROR_CODE).send({ message: 'Передан некорректный _id карточки' });
+    //     return;
+    //   }
+    //   if (err.name === 'ValidationError') {
+    //     res.status(BAD_REQUEST_ERROR_CODE).send({ message: 'Переданы некорректные данные для постановки лайка' });
+    //     return;
+    //   }
+    //   res.status(INTERNAL_SERVER_ERROR_CODE).send({ message: 'Произошла неизвестная ошибка на сервере' });
+    // });
 };
 
-const dislikeCard = (req, res) => {
+const dislikeCard = (req, res, next) => {
   const userId = req.user._id;
   const { cardId } = req.params;
 
@@ -91,21 +123,22 @@ const dislikeCard = (req, res) => {
     .then((card) => {
       res.send({ data: card });
     })
-    .catch((err) => {
-      if (err.name === 'DocumentNotFoundError') {
-        res.status(NOT_FOUND_ERROR_CODE).send({ message: 'Передан несуществующий _id карточки' });
-        return;
-      }
-      if (err.name === 'CastError') {
-        res.status(BAD_REQUEST_ERROR_CODE).send({ message: 'Передан некорректный _id карточки' });
-        return;
-      }
-      if (err.name === 'ValidationError') {
-        res.status(BAD_REQUEST_ERROR_CODE).send({ message: 'Переданы некорректные данные для снятия лайка' });
-        return;
-      }
-      res.status(INTERNAL_SERVER_ERROR_CODE).send({ message: 'Произошла неизвестная ошибка на сервере' });
-    });
+    .catch(next);
+    // .catch((err) => {
+    //   if (err.name === 'DocumentNotFoundError') {
+    //     res.status(NOT_FOUND_ERROR_CODE).send({ message: 'Передан несуществующий _id карточки' });
+    //     return;
+    //   }
+    //   if (err.name === 'CastError') {
+    //     res.status(BAD_REQUEST_ERROR_CODE).send({ message: 'Передан некорректный _id карточки' });
+    //     return;
+    //   }
+    //   if (err.name === 'ValidationError') {
+    //     res.status(BAD_REQUEST_ERROR_CODE).send({ message: 'Переданы некорректные данные для снятия лайка' });
+    //     return;
+    //   }
+    //   res.status(INTERNAL_SERVER_ERROR_CODE).send({ message: 'Произошла неизвестная ошибка на сервере' });
+    // });
 };
 
 module.exports = {
--- controllers/users.js
+++ controllers/users.js
@@ -1,59 +1,114 @@
+const bcrypt = require('bcryptjs');
+
+const jwt = require('jsonwebtoken');
+
 const {
   CREATED_CODE,
   BAD_REQUEST_ERROR_CODE,
+  UNAUTHORIZED_ERROR_CODE,
   NOT_FOUND_ERROR_CODE,
+  CONFLICT_ERROR_CODE,
   INTERNAL_SERVER_ERROR_CODE,
 } = require('../constants/constants');
 
+const AuthorizationError = require('../errors/AuthorizationError');
+
 const User = require('../models/users');
 
-const getUsers = (req, res) => {
+const getUsers = (req, res, next) => {
   User.find()
     .then((users) => {
       res.send({ data: users });
     })
-    .catch(() => {
-      res.status(INTERNAL_SERVER_ERROR_CODE).send({ message: 'Произошла неизвестная ошибка на сервере' });
-    });
+    .catch(next);
+    // .catch(() => {
+    //   res.status(INTERNAL_SERVER_ERROR_CODE).send({ message: 'Произошла неизвестная ошибка на сервере' });
+    // });
 };
 
-const getUser = (req, res) => {
+const getUserMe = (req, res, next) => {
+  const userId = req.user._id;
+  User.findById(userId)
+    .orFail()
+    .then((user) => {
+      res.send({ data: user });
+    })
+    .catch(next);
+    // .catch((err) => {
+    //   if (err.name === 'CastError') {
+    //     res.status(BAD_REQUEST_ERROR_CODE).send({ message: 'Передан некорректный _id пользователя' });
+    //     return;
+    //   }
+    //   if (err.name === 'DocumentNotFoundError') {
+    //     res.status(NOT_FOUND_ERROR_CODE).send({ message: 'Пользователь с указанным _id не найден' });
+    //     return;
+    //   }
+    //   res.status(INTERNAL_SERVER_ERROR_CODE).send({ message: 'Произошла неизвестная ошибка на сервере' });
+    // });
+};
+
+const getUser = (req, res, next) => {
   const { userId } = req.params;
   User.findById(userId)
     .orFail()
     .then((user) => {
       res.send({ data: user });
     })
-    .catch((err) => {
-      if (err.name === 'CastError') {
-        res.status(BAD_REQUEST_ERROR_CODE).send({ message: 'Передан некорректный _id пользователя' });
-        return;
-      }
-      if (err.name === 'DocumentNotFoundError') {
-        res.status(NOT_FOUND_ERROR_CODE).send({ message: 'Пользователь с указанным _id не найден' });
-        return;
-      }
-      res.status(INTERNAL_SERVER_ERROR_CODE).send({ message: 'Произошла неизвестная ошибка на сервере' });
-    });
+    .catch(next);
+    // .catch((err) => {
+    //   if (err.name === 'CastError') {
+    //     res.status(BAD_REQUEST_ERROR_CODE).send({ message: 'Передан некорректный _id пользователя' });
+    //     return;
+    //   }
+    //   if (err.name === 'DocumentNotFoundError') {
+    //     res.status(NOT_FOUND_ERROR_CODE).send({ message: 'Пользователь с указанным _id не найден' });
+    //     return;
+    //   }
+    //   res.status(INTERNAL_SERVER_ERROR_CODE).send({ message: 'Произошла неизвестная ошибка на сервере' });
+    // });
 };
 
-const createUser = (req, res) => {
-  const { name, about, avatar } = req.body;
+const createUser = (req, res, next) => {
+  const {
+    email,
+    password,
+    name,
+    about,
+    avatar,
+  } = req.body;
 
-  User.create({ name, about, avatar })
-    .then((newUser) => {
-      res.status(CREATED_CODE).send({ data: newUser });
+  bcrypt.hash(password, 10)
+    .then((hash) => {
+      User.create({
+        email,
+        password: hash,
+        name,
+        about,
+        avatar,
+      })
+        .then((newUser) => {
+          res.status(CREATED_CODE).send({ data: newUser });
+        })
+        .catch(next);
+        // .catch((err) => {
+        //   if (err.name === 'ValidationError') {
+        //     res.status(BAD_REQUEST_ERROR_CODE).send({ message: 'Переданы некорректные данные при создании пользователя' });
+        //     return;
+        //   }
+        //   if (err.code === 11000) {
+        //     res.status(CONFLICT_ERROR_CODE).send({ message: 'Пользователь с таким E-mail уже существует' });
+        //     return;
+        //   }
+        //   res.status(INTERNAL_SERVER_ERROR_CODE).send({ message: 'Произошла неизвестная ошибка на сервере' });
+        // });
     })
-    .catch((err) => {
-      if (err.name === 'ValidationError') {
-        res.status(BAD_REQUEST_ERROR_CODE).send({ message: 'Переданы некорректные данные при создании пользователя' });
-      } else {
-        res.status(INTERNAL_SERVER_ERROR_CODE).send({ message: 'Произошла неизвестная ошибка на сервере' });
-      }
-    });
+    .catch(next);
+    // .catch(() => {
+    //   res.status(INTERNAL_SERVER_ERROR_CODE).send({ message: 'Произошла неизвестная ошибка на сервере' });
+    // });
 };
 
-const updateUserData = (req, res) => {
+const updateUserData = (req, res, next) => {
   const { name, about } = req.body;
   const userId = req.user._id;
 
@@ -62,24 +117,25 @@ const updateUserData = (req, res) => {
     .then((user) => {
       res.send({ data: user });
     })
-    .catch((err) => {
-      if (err.name === 'CastError') {
-        res.status(BAD_REQUEST_ERROR_CODE).send({ message: 'Передан некорректный _id пользователя' });
-        return;
-      }
-      if (err.name === 'DocumentNotFoundError') {
-        res.status(NOT_FOUND_ERROR_CODE).send({ message: 'Пользователь с указанным _id не найден' });
-        return;
-      }
-      if (err.name === 'ValidationError') {
-        res.status(BAD_REQUEST_ERROR_CODE).send({ message: 'Переданы некорректные данные при обновлении профиля' });
-        return;
-      }
-      res.status(INTERNAL_SERVER_ERROR_CODE).send({ message: 'Произошла неизвестная ошибка на сервере' });
-    });
+    .catch(next);
+    // .catch((err) => {
+    //   if (err.name === 'CastError') {
+    //     res.status(BAD_REQUEST_ERROR_CODE).send({ message: 'Передан некорректный _id пользователя' });
+    //     return;
+    //   }
+    //   if (err.name === 'DocumentNotFoundError') {
+    //     res.status(NOT_FOUND_ERROR_CODE).send({ message: 'Пользователь с указанным _id не найден' });
+    //     return;
+    //   }
+    //   if (err.name === 'ValidationError') {
+    //     res.status(BAD_REQUEST_ERROR_CODE).send({ message: 'Переданы некорректные данные при обновлении профиля' });
+    //     return;
+    //   }
+    //   res.status(INTERNAL_SERVER_ERROR_CODE).send({ message: 'Произошла неизвестная ошибка на сервере' });
+    // });
 };
 
-const updateUserAvatar = (req, res) => {
+const updateUserAvatar = (req, res, next) => {
   const { avatar } = req.body;
   const userId = req.user._id;
 
@@ -88,27 +144,63 @@ const updateUserAvatar = (req, res) => {
     .then((user) => {
       res.send({ data: user });
     })
-    .catch((err) => {
-      if (err.name === 'CastError') {
-        res.status(BAD_REQUEST_ERROR_CODE).send({ message: 'Передан некорректный _id пользователя' });
-        return;
+    .catch(next);
+    // .catch((err) => {
+    //   if (err.name === 'CastError') {
+    //     res.status(BAD_REQUEST_ERROR_CODE).send({ message: 'Передан некорректный _id пользователя' });
+    //     return;
+    //   }
+    //   if (err.name === 'DocumentNotFoundError') {
+    //     res.status(NOT_FOUND_ERROR_CODE).send({ message: 'Пользователь с указанным _id не найден' });
+    //     return;
+    //   }
+    //   if (err.name === 'ValidationError') {
+    //     res.status(BAD_REQUEST_ERROR_CODE).send({ message: 'Переданы некорректные данные при обновлении аватара' });
+    //     return;
+    //   }
+    //   res.status(INTERNAL_SERVER_ERROR_CODE).send({ message: 'Произошла неизвестная ошибка на сервере' });
+    // });
+};
+
+const login = (req, res, next) => {
+  const {
+    email,
+    password,
+  } = req.body;
+
+  User.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        // res.status(UNAUTHORIZED_ERROR_CODE).send({ message: 'Неправильные почта или пароль' });
+        // return;
+        return next(new AuthorizationError('Неправильные почта или пароль'));
       }
-      if (err.name === 'DocumentNotFoundError') {
-        res.status(NOT_FOUND_ERROR_CODE).send({ message: 'Пользователь с указанным _id не найден' });
-        return;
-      }
-      if (err.name === 'ValidationError') {
-        res.status(BAD_REQUEST_ERROR_CODE).send({ message: 'Переданы некорректные данные при обновлении аватара' });
-        return;
-      }
-      res.status(INTERNAL_SERVER_ERROR_CODE).send({ message: 'Произошла неизвестная ошибка на сервере' });
-    });
+
+      bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            // res.status(UNAUTHORIZED_ERROR_CODE).send({ message: 'Неправильные почта или пароль' });
+            // return;
+            return next(new AuthorizationError('Неправильные почта или пароль'));
+          }
+
+          const token = jwt.sign({ _id: user._id }, 'SECRET_KEY', { expiresIn: '7d' }); // HARDCODE SECRET_KEY
+          // eslint-disable-next-line consistent-return
+          return res.send({ JWT: token });
+        });
+    })
+    .catch(next);
+    // .catch(() => {
+    //   res.status(INTERNAL_SERVER_ERROR_CODE).send({ message: 'Произошла неизвестная ошибка на сервере' });
+    // });
 };
 
 module.exports = {
   getUsers,
   getUser,
-  createUser,
+  getUserMe,
   updateUserData,
   updateUserAvatar,
+  createUser,
+  login,
 };
